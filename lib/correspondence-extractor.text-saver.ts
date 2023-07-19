66e9f60c05a41dada9bc1e1ca9f24dc1ee8dc22f
Christian Angelone <christian.angelone@caylent.com>
2023-07-19 16:35:03 -0300

    Adds correspondence extractor

---
+++ lib/correspondence-extractor.text-saver.ts
@@ -0,0 +1,34 @@
+import { Handler } from 'aws-cdk-lib/aws-lambda';
+import { TriggerEvent } from './shared';
+import { TextExtractor, TextractRecord } from './text-extractor';
+import { DynamoDBPersistor } from './dynamodb-persistor';
+import { SNSEvent } from 'aws-lambda';
+
+interface Result {
+  status: string
+}
+
+export const handler: Handler = async (
+  event: SNSEvent
+): Promise<any> => {
+  console.log('Got SNS Event', event);
+  const results = event.Records.map(async (record) => {
+    const jobData = JSON.parse(record.Sns.Message) as TextractRecord;
+    const documentId = jobData.JobTag;
+    const jobId = jobData.JobId;
+
+    const extractor = new TextExtractor({});
+    const extraction = await extractor.fetchJobOutputFrom({ jobId });
+
+    const persistor = new DynamoDBPersistor();
+    const status = await persistor.persist(process.env.DOC_INFO_TABLE_NAME, documentId, {
+        type: { S: 'correspondence'},
+        rawText: {
+            L: extraction.map((line) => ({ S: line })),
+        }
+    });
+    return status;
+  });
+
+  return Promise.allSettled(results);
+}
---
+++ lib/text-extractor.ts
@@ -0,0 +1,123 @@
+import { v4 as uuid } from 'uuid';
+import {
+    DetectDocumentTextCommand,
+    GetDocumentTextDetectionCommand,
+    GetDocumentTextDetectionCommandOutput,
+    StartDocumentTextDetectionCommand,
+    TextractClient,
+} from '@aws-sdk/client-textract';
+  
+const textract = new TextractClient({});
+
+export interface TextExtractorAsyncResult {
+    documentId?: string;
+    jobId?: string;
+}
+
+export interface TextExtractorSyncResult {
+    documentId: string;
+    extraction: string[]
+}
+
+export interface TextractRecord {
+    JobId: string;
+    Status: string;
+    API: string;
+    JobTag: string;
+    Timestamp: number;
+    DocumentLocation: {
+      S3ObjectName: string;
+      S3Bucket: string;
+    };
+  }
+
+export class TextExtractor {
+    notify: {topicArn?: string, roleArn?: string};
+
+    constructor(notify: {topicArn?: string, roleArn?: string}) {
+        this.notify = notify;
+    }
+
+    // Only for .jpg or .png files (for .pdf use async)
+    async syncExtract(bucket: string, key: string): Promise<TextExtractorSyncResult> {
+        const documentId = uuid();
+        const extraction = await textract.send(
+            new DetectDocumentTextCommand({
+                Document: {
+                    S3Object: {
+                      Bucket: bucket,
+                      Name: key
+                    },
+                },
+            })
+        );
+
+        const extractedLines = extraction.Blocks
+            ?.filter(block => block.BlockType === 'LINE' && block.Text)
+            ?.map(block => block.Text!) || [];
+        
+        return {
+            documentId,
+            extraction: extractedLines
+        }
+    }
+
+    async asyncExtract(bucket: string, key: string): Promise<TextExtractorAsyncResult> {
+        if (!this.notify.roleArn)
+            throw Error('Missing notify roleArn')
+
+        if (!this.notify.topicArn)
+            throw Error('Missing notify topicArn')
+
+        const documentId = uuid();
+
+        const extractTextJob = await textract.send(
+            new StartDocumentTextDetectionCommand({
+                DocumentLocation: {
+                    S3Object: {
+                        Bucket: bucket,
+                        Name: key,
+                    },
+                },
+                ClientRequestToken: documentId,
+                NotificationChannel: {
+                    RoleArn: this.notify.roleArn,
+                    SNSTopicArn: this.notify.topicArn,
+                },
+                JobTag: documentId,
+            })
+        );
+        return {
+            documentId,
+            jobId: extractTextJob.JobId,
+        };
+    }
+
+    async fetchJobOutputFrom(result: TextExtractorAsyncResult): Promise<string[]> {
+        if (!result.jobId) {
+            throw new Error('missing job id')
+        }
+
+        let nextToken = undefined;
+        let text: string[] = [];
+        do {
+          console.debug('Getting Text Result', nextToken);
+          const extraction: GetDocumentTextDetectionCommandOutput = await textract.send(
+            new GetDocumentTextDetectionCommand({
+              JobId: result.jobId,
+              NextToken: nextToken,
+            })
+          );
+          nextToken = extraction.NextToken;
+      
+          console.debug('Got', extraction.Blocks?.length, 'blocks');
+          const lines = extraction.Blocks
+            ?.filter(block => block.BlockType === 'LINE' && block.Text)
+            ?.map(block => block.Text!) || [];
+        
+          text = text.concat(...lines);
+        } while (nextToken);
+        
+        return text;
+    }
+}
